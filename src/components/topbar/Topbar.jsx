37502fa4f33f23e56fc59395136246c15d3308c9
Extension1990 <extensiontlhareseng@gmail.com>
2021-05-21 17:33:37 +0200

    completed project

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import './topbar.scss';
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
 
-const Topbar = () => {
-    return (
-        <div className="topbar">
-            
+export default function Topbar({ menuOpen, setMenuOpen }) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            Extension
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>+27 74 681 9382</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>extensiontlhareseng@gmail.com</span>
+          </div>
         </div>
-    )
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-export default Topbar
